4352e9222e5ed5df1a6a24508f44fdf01ef6bd9f
Ihar-Batura <A17331582@gmail.com>
2025-09-03 18:07:04 +0300

    feat: add ThemeProvider to layout

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,15 @@
 import { useTranslations } from 'next-intl';
+import { Header } from '@/components/layout/header/Header';
 
 export default function Home() {
   const t = useTranslations('Header');
 
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1>{t('hello')}</h1>
-    </div>
+    <>
+      <Header />
+      <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+        <h1>{t('hello')}</h1>
+      </div>
+    </>
   );
 }
