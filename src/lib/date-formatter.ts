975e328d203c4cbf62d66aaa2470304470b9f8aa
Alena <morven2018@yandex.ru>
2025-09-05 07:08:20 +0300

    feat: add info message utils

---
+++ src/lib/date-formatter.ts
@@ -0,0 +1,11 @@
+const dateString = (date: Date = new Date()): string =>
+  date.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: '2-digit',
+    hour12: true,
+  });
+
+export default dateString;
