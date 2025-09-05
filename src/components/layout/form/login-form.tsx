2655205a35853823d51d881d9eb60cf6c039535c
Alena <morven2018@yandex.ru>
2025-09-06 00:01:39 +0300

    feat: add handler to sonner msg and add itnl to it

--- src/components/layout/form/login-form.tsx
+++ src/components/layout/form/login-form.tsx
@@ -12,11 +12,13 @@ import { Label } from '@/components/ui/label';
 import { PasswordInput } from '@/components/ui/password-input';
 import { toastError, toastSuccess } from '@/components/ui/sonner';
 import { useAuth } from '@/context/auth/auth-context';
+import { useLogout } from '@/hooks/use-logout';
 import { getAuthErrorInfo } from '@/lib/error-handlers/error-message';
 
 export const LoginForm = () => {
   const t = useTranslations('Login');
   const te = useTranslations('ValidationErrors');
+  const handleLogout = useLogout();
   const schema = loginSchema(te);
   const { login } = useAuth();
   const router = useRouter();
@@ -44,37 +46,26 @@ export const LoginForm = () => {
       setAuthError('');
 
       const token = await login(data.email, data.password);
-      toastSuccess('You have successfully logged in.', {
+      toastSuccess(t('success'), {
         action: {
-          label: 'LOG OUT',
-          onClick: () => {
-            console.log('Logout clicked');
-          },
+          label: t('logout-btn'),
+          onClick: () => handleLogout(),
         },
       });
-
-      console.log('Auth successful. Token:', token);
 
       router.push('/');
     } catch (error) {
       const authErrorInfo = getAuthErrorInfo(error);
-      if (authErrorInfo.isInvalidCredentials)
-        toastError('No user exist with such parameters');
+      if (authErrorInfo.isInvalidCredentials) toastError(t('no-user'));
       else
-        toastError(
-          `An error occurred while logging in. Error: ${authErrorInfo.message}`,
-          {
-            action: {
-              label: 'RETRY',
-              onClick: () => {
-                console.log('Retry clicked');
-                handleSubmit(onSubmit)();
-              },
+        toastError(`${t('error')} ${authErrorInfo.message}`, {
+          action: {
+            label: t('retry-btn'),
+            onClick: () => {
+              handleSubmit(onSubmit)();
             },
-          }
-        );
-
-      console.error('Auth error:', error);
+          },
+        });
       setAuthError('error');
     }
   };
@@ -112,7 +103,7 @@ export const LoginForm = () => {
 
       <div className="grid gap-3">
         <Label htmlFor="password" className="text-base">
-          Password
+          {t('password')}
         </Label>
         <Controller
           name="password"
