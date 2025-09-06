70ea74a1240de918587bb388de21ce503f66d096
Alena <morven2018@yandex.ru>
2025-09-06 22:29:02 +0300

    fix: update env config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,15 +3,17 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_API_KEY || '',
+  apiKey: process.env.NEXT_PUBLIC_API_KEY || '',
   authDomain:
-    process.env.NEXT_AUTH_DOMAIN || 'rest-client-app-c8844.firebaseapp.com',
-  projectId: process.env.NEXT_PROJECT_ID || 'rest-client-app-c8844',
+    process.env.NEXT_PUBLIC_AUTH_DOMAIN ||
+    'rest-client-app-c8844.firebaseapp.com',
+  projectId: process.env.NEXT_PUBLIC_ROJECT_ID || 'rest-client-app-c8844',
   storageBucket:
-    process.env.NEXT_AUTH_DOMAIN || 'rest-client-app-c8844.firebasestorage.app',
-  messagingSenderId: process.env.NEXT_SENDER_ID || '',
-  appId: process.env.NEXT_APP_ID || '',
-  measurementId: process.env.NEXT_MEASUREMENT_ID || '',
+    process.env.NEXT_PUBLIC_AUTH_DOMAIN ||
+    'rest-client-app-c8844.firebasestorage.app',
+  messagingSenderId: process.env.NEXT_PUBLIC_SENDER_ID || '',
+  appId: process.env.NEXT_PUBLIC_APP_ID || '',
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID || '',
 };
 
 const app = initializeApp(firebaseConfig);
