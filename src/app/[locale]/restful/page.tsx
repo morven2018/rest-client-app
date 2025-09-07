cf79e77f2cda8200bdd5df10d8c8501cd6120eba
Ihar-Batura <A17331582@gmail.com>
2025-09-07 11:44:02 +0300

    feat: add new components

--- src/app/[locale]/restful/page.tsx
+++ src/app/[locale]/restful/page.tsx
@@ -1,7 +1,9 @@
 export default function RestfulPage() {
   return (
-    <main>
-      <div>This is Restful page</div>
+    <main className="w-full">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-[17px]">
+        <div className="flex gap-2"></div>
+      </div>
     </main>
   );
 }
