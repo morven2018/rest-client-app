7da978a196503b91fbc1323cd272ea4090a47204
Alena <morven2018@yandex.ru>
2025-09-09 21:25:45 +0300

    fix: remove merge conflict

--- src/app/[locale]/variables/page.tsx
+++ src/app/[locale]/variables/page.tsx
@@ -1,22 +1,14 @@
-<<<<<<< HEAD
 import CustomSidebar from '@/components/layout/sidebar/sidebar';
-=======
 import Heading from '@/components/layout/breadcrumb-and-heading/heading';
->>>>>>> origin/develop
 
 export default function VariablesPage() {
   return (
     <main>
-<<<<<<< HEAD
       <CustomSidebar>
-        <div>This is Variables page</div>
+        <Heading>
+          <div>This is Variables page</div>
+        </Heading>
       </CustomSidebar>
-=======
-      <div>This is Variables page</div>
-      <Heading>
-        <div>dvdsv</div>
-      </Heading>
->>>>>>> origin/develop
     </main>
   );
 }
