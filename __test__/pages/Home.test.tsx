5bd77c553628c31562594035387b3c1bc7f9ffbe
Alena <morven2018@yandex.ru>
2025-09-11 22:12:51 +0300

    feat: add mock of server

--- __test__/pages/Home.test.tsx
+++ __test__/pages/Home.test.tsx
@@ -1,12 +1,14 @@
-import { render } from '@testing-library/react';
 import Home from '@/app/[locale]/page';
+import { render } from '@testing-library/react';
 
-jest.mock('next-intl', () => ({
-  useTranslations: () => (key: string) => key,
-}));
+jest.mock('../__mocks__/next-intl/server');
 
 jest.mock('@/components/layout/header/Header', () => ({
   Header: () => <div>Header Mock</div>,
+}));
+
+jest.mock('@/components/layout/team/teamWrapper', () => ({
+  TeamWrapper: () => <div>Team Mock</div>,
 }));
 
 describe('Home', () => {
