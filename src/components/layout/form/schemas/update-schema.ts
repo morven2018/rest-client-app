72d241f1eb1518273476348149ef09d2c9157cb5
Alena <morven2018@yandex.ru>
2025-09-13 16:21:04 +0300

    refactor: remove duplication

--- src/components/layout/form/schemas/update-schema.ts
+++ src/components/layout/form/schemas/update-schema.ts
@@ -1,27 +1,8 @@
 import { z } from 'zod';
+import { avatarSchema, usernameSchema } from './common-schemas';
 
 export const updateAccountSchema = (t: (key: string) => string) =>
   z.object({
-    username: z
-      .string()
-      .optional()
-      .refine((val) => !val || val[0]?.toUpperCase() === val[0], {
-        message: t('name-uppercase'),
-      })
-      .refine((val) => !val || (val.length >= 3 && val.length <= 20), {
-        message: t('name-length'),
-      })
-      .refine((val) => !val || /^[a-zA-Zа-яА-Я\d_ ]+$/.test(val), {
-        message: t('name-format'),
-      }),
-
-    avatar: z
-      .instanceof(File)
-      .optional()
-      .refine(
-        (file) =>
-          !file || (file instanceof File && file.type.startsWith('image/')),
-        t('image-only')
-      )
-      .refine((file) => !file || file.size <= 2 * 1024 * 1024, t('image-size')),
+    username: usernameSchema(t),
+    avatar: avatarSchema(t),
   });
