4d2566f50d55a8d04ae9ea34537816d0a50207aa
Alena <morven2018@yandex.ru>
2025-09-14 20:40:19 +0300

    fix: remove linter issues

--- src/context/auth/auth-provider.tsx
+++ src/context/auth/auth-provider.tsx
@@ -18,14 +18,6 @@ import {
 const AUTH_TOKEN_KEY = 'authToken';
 const CHECK_FREQUENCY = 30000;
 
-interface UpdateProfileData {
-  username?: string;
-  avatar?: File;
-  email?: string;
-  password?: string;
-  currentPassword?: string;
-}
-
 export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
