d827fa3a797bda4904a2367b6ced32e4f603420a
Alena <morven2018@yandex.ru>
2025-09-16 00:32:52 +0300

    fix: linter issue

--- __test__/__mocks__/next-intl/routing.ts
+++ __test__/__mocks__/next-intl/routing.ts
@@ -1,6 +1,5 @@
 export const defineRouting = jest.fn().mockImplementation((config) => config);
 
-// Для использования в тестах
 export const mockRoutingConfig = {
   locales: ['en', 'ru'] as const,
   defaultLocale: 'en' as const,
