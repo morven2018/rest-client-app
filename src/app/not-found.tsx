27db4d27a9ee6575badee81243881678c1b22e61
Ihar-Batura <A17331582@gmail.com>
2025-09-16 16:52:23 +0300

    feat: add some styles for base page NotFound

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,10 +2,15 @@ import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <main>
-      <h2>Oooops!</h2>
-      <h2>This page is not found</h2>
-      <Link href="/">Home</Link>
+    <main className="flex flex-col items-center justify-center min-h-screen gap-5">
+      <h2 className="text-red-600 font-bold text-5xl">Oooops!</h2>
+      <h2 className="text-xl">This page is not found</h2>
+      <Link
+        href="/"
+        className="bg-purple-800 text-white hover:bg-purple-900 px-10 py-3 rounded-lg font-semibold "
+      >
+        Home
+      </Link>
     </main>
   );
 }
